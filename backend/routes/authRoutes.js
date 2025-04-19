1a3ae73b87f2b6a7a696902a9d192361be668343
NImnaka <10899636@students.plymouth.ac.uk>
2025-04-20 01:13:35 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { registerUser, loginUser } from '../controllers/authController.js';
+import {
+    registerUser,
+    loginUser,
+    getUserProfile,
+    updateUserProfile,
+  } from "../controllers/authController.js";
+  import { authMiddleware } from "../middleware/authMiddleware.js"; 
 
 const router = express.Router();
 
 // Routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", authMiddleware, getUserProfile);
+router.put("/update", authMiddleware, updateUserProfile);
+
 
 export default router;
+
