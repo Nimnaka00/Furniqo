0c917ae5652ff5fc1c53e8813379b457bca1f433
NImnaka <10899636@students.plymouth.ac.uk>
2025-04-19 21:30:21 +0530

    Update products.js

---
+++ furniqo/src/data/products.js
@@ -0,0 +1,29 @@
+const products = {
+    chairs: [
+      { id: 1, name: "Cozy Chair", price: "$120", image: "/assets/furniture/chair1.png" },
+      { id: 2, name: "Retro Chair", price: "$110", image: "/assets/furniture/chair2.png" },
+      { id: 3, name: "Comfy Chair", price: "$150", image: "/assets/furniture/chair3.png" },
+      { id: 4, name: "Elegant Chair", price: "$130", image: "/assets/furniture/chair4.png" },
+    ],
+    beds: [
+      { id: 1, name: "Queen Bed", price: "$400", image: "/assets/furniture/bed1.png" },
+      { id: 2, name: "King Bed", price: "$520", image: "/assets/furniture/bed2.png" },
+      { id: 3, name: "Simple Bed", price: "$350", image: "/assets/furniture/bed3.png" },
+      { id: 4, name: "Luxury Bed", price: "$600", image: "/assets/furniture/bed4.png" },
+    ],
+    sofas: [
+      { id: 1, name: "Modern Sofa", price: "$270", image: "/assets/furniture/sofa1.png" },
+      { id: 2, name: "Classic Sofa", price: "$300", image: "/assets/furniture/sofa2.png" },
+      { id: 3, name: "Family Sofa", price: "$350", image: "/assets/furniture/sofa3.png" },
+      { id: 4, name: "Small Sofa", price: "$250", image: "/assets/furniture/sofa4.png" },
+    ],
+    lamps: [
+      { id: 1, name: "Desk Lamp", price: "$40", image: "/assets/furniture/lamp1.png" },
+      { id: 2, name: "Standing Lamp", price: "$60", image: "/assets/furniture/lamp2.png" },
+      { id: 3, name: "Wall Lamp", price: "$50", image: "/assets/furniture/lamp3.png" },
+      { id: 4, name: "Ceiling Lamp", price: "$70", image: "/assets/furniture/lamp4.png" },
+    ],
+  };
+  
+  export default products;
+  
