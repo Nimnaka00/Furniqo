6c6d696b4267107f62a732f26da35d51ebb5da53
NImnaka <10899636@students.plymouth.ac.uk>
2025-04-20 01:13:40 +0530

    Update App.jsx

--- furniqo/src/App.jsx
+++ furniqo/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
-import Signup from "./pages/Signup"; // ✅ Import Signup
+import Signup from "./pages/Signup"; 
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} /> {/* ✅ Signup route */}
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
