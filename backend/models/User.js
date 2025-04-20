2e047e5cc686b9e01c1858dd956af86a790a3782
NImnaka <10899636@students.plymouth.ac.uk>
2025-04-20 18:43:49 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,6 +18,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       minlength: 6
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user' // normal users get this automatically
     }
   },
   { timestamps: true }
