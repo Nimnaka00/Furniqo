c57caadc8e1709730a6deaae47189a8617a98100
NImnaka <10899636@students.plymouth.ac.uk>
2025-04-25 21:01:55 +0530

    Update useInViewAnimation.js

---
+++ furniqo/src/hooks/useInViewAnimation.js
@@ -0,0 +1,20 @@
+import { useEffect, useState, useRef } from "react";
+
+export const useInViewAnimation = () => {
+  const ref = useRef();
+  const [inView, setInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setInView(entry.isIntersecting);
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return [ref, inView];
+};
